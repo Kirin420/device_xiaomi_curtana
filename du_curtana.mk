a5989ced36490eaae310d341bbc954aa3850f422
teamsolo <teamslow8218@gmail.com>
2020-07-23 12:38:50 +0000

    curtana: du-ify * bring up for dirty unicorn

--- du_curtana.mk
+++ du_curtana.mk
@@ -12,16 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from curtana device
 $(call inherit-product, device/xiaomi/curtana/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common DirtyUnicorn stuff.
 CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_curtana
+PRODUCT_NAME := du_curtana
 PRODUCT_DEVICE := curtana
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 9S
